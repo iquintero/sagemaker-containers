381321f9314495e988d00c8f7d904edeb8405dbe
Ignacio Quintero <rignacio@amazon.com>
2018-05-04 15:01:27 -0700

    Fix flake8

--- test/unit/test_trainer.py
+++ test/unit/test_trainer.py
@@ -10,9 +10,9 @@
 # distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
-from mock import Mock, patch, PropertyMock
+from mock import Mock, patch
 
-from sagemaker_containers import env, trainer
+from sagemaker_containers import trainer
 
 
 class TrainingEnv(Mock):
